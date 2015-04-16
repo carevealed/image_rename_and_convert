c31c1021bdeb32f38d300a3aa0fad7de454ee4e0
henryborchers <henryborchers@yahoo.com>
2015-04-16 15:57:32 -0700

    added database building functions

--- rename_files/cli.py
+++ rename_files/cli.py
@@ -9,20 +9,29 @@ def main(source):
     builder = rename_files.renaming_model.RenameFactory()
     if debug:
         print(source)
+
     for root, subdirs, files in os.walk(source):
         for file in files:
             if os.path.splitext(file)[1] == '.tif':
                 newfile = os.path.join(root, file)
-                builder.add_queue(newfile)
-    if debug:
-        print(builder.__dict__)
-        print(len(builder))
+                builder.add_queue(newfile, "caps", 1)
 
+    reporter = rename_files.renaming_model.ReportFactory()
+    reporter.initize_database()
+    for i in builder:
+        record = builder.execute_rename_from_queue_by_record(i)
+        reporter.add_record(record)
+
+    # print(type(reporter))
+    reporter.show_current_records(object_id="dsafasdf")
+    reporter.close_database()
+    # for queue in builder.queues:
+    #     print(queue)
 
 
 if __name__ == "__main__":
+    if debug:
+        sys.stderr.write("Don't forget to get rid of this!!!\n")
+        main("/Volumes/CAVPPTestDrive/DPLA0003/Images/_Anaheim/")
     sys.stderr.write("Not a main entry point\n")
-    sys.stderr.write("Don't forget to get rid of this!!!\n")
-
-
-    main("/Volumes/CAVPPTestDrive/DPLA0003/Images/_Anaheim/")
+    sys.stdout.flush()
