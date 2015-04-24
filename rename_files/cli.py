b4d4dba74c34ea7b773aea266c33b4ede85853f0
henryborchers <henryborchers@yahoo.com>
2015-04-23 17:38:20 -0700

    added validation for all required values in CLI and ask for assurance from user.

--- rename_files/cli.py
+++ rename_files/cli.py
@@ -15,40 +15,279 @@ class running_mode(Enum):
 MODE = running_mode.DEBUG
 
 
-def start_cli(source=None,
-              obj_id_prefix=None,
-              obj_id_start_num=None,
-              proj_id_prefix=None,
-              proj_id_start_num=None,
-              output_path=None,
-              user=None):
+def is_reponse_valid(response):
+    if response.lower() == "q" or response.lower() == "quit":
+        print("Quiting")
+        quit()
+    return True
 
-    if obj_id_prefix:
-        object_id_prefix = obj_id_prefix
-    else:
-        object_id_prefix = None
 
-    if obj_id_start_num:
-        first_object_id = obj_id_start_num
-    else:
-        first_object_id = 0
+def is_valid_folder_name(response):
+    # TODO: build a function to check if a the folder name is accurate
+    return True
+    pass
 
-    if proj_id_prefix:
-        proj_id_prefix = proj_id_prefix
-    else:
-        proj_id_prefix = "CAPS"
 
-    if proj_id_start_num:
-        first_project_id = proj_id_start_num
-    else:
-        first_project_id = 0
+def valid_str(key, keyname, kwargs):
+    valid = False
+    while not valid:
+        if key in kwargs:
+            if kwargs[key]:
+                if isinstance(kwargs[key], str):
+                    print("{} {}".format("{}:".format(keyname), "[{}]".format(kwargs[key])), end="")
+                    response = input(" :").strip()
+                    if response:
+                        if not isinstance(response,str):
+                            sys.stderr.write("This data must be made up of letters.\n")
+                            continue
+                        if is_reponse_valid(response):
+                            # kwargs[key] = response
+                            valid = True
+                            return response
+                        else:
+                            valid = False
+                            continue
+                    else:
+                        valid = True
+                        return kwargs[key]
 
+            valid = True
+        else:
+            print("{}".format(keyname), end="")
+            response = input(" :").strip()
+            if response:
+                if not isinstance(response,str):
+                    sys.stderr.write("This data must be made up of letters.\n")
+                    continue
+                if is_reponse_valid(response):
+                    # kwargs[key] = response
+                    valid = True
+                    return response
+                else:
+                    valid = False
+                    continue
+            else:
+                valid = False
+                continue
+
+def valid_int(key, keyname, kwargs):
+    valid = False
+    while not valid:
+        if key in kwargs:
+            if kwargs[key]:
+                int(kwargs[key])
+                print("{}: {}".format(keyname, "[{}]".format(kwargs[key])), end="")
+                response = input(" :").strip()
+                if response:
+                    try:
+                        int(response)
+                    except ValueError:
+                        sys.stderr.write("This data must be an integer number.\n")
+                        continue
+                    if is_reponse_valid(response):
+                        # kwargs[key] = response
+                        valid = True
+                        return response
+                    else:
+                        valid = False
+                        sys.stderr.write("This information is required. Error code variation: 1.\n")
+                        continue
+                else:
+                    valid = True
+                    return kwargs[key]
+
+        print("{}".format(keyname), end="")
+        response = input(" :").strip()
+        if response:
+            try:
+                int(response)
+            except ValueError:
+                sys.stderr.write("This data must be an integer number.\n")
+                continue
+            if is_reponse_valid(response):
+                # kwargs[key] = response
+                valid = True
+                return response
+            else:
+                valid = False
+                sys.stderr.write("This information is required. Error code variation: 2.\n")
+                continue
+        else:
+            valid = False
+            sys.stderr.write("This information is required. Error code variation: 3.\n")
+            continue
+
+
+
+def update_information(kwargs):
+
+    print("This script needs some information before it can run. Please enter or update the following information. \n"
+          "If a value is already suggested, you can confirm by pressing return without entering any values. \n"
+          "To quit at any point, type \"q\" or \"quit\" and press enter. ")
+    if MODE == running_mode.DEBUG:
+        print("\n***** DEBUG INFORMATION ***** ")
+        for keys,values in kwargs.items():
+            print("{:<20}:{:<20}".format(keys, str(values)))
+        print("***** DEBUG INFORMATION *****\n\n")
+
+
+    valid = False
+    while not valid:
+        if 'source' in kwargs:
+            print("Source: [{}]".format(kwargs['source']), end="")
+            valid = True
+        else:
+            print("Source ", end="")
+        response = input(" :").strip()
+        if response:
+            if is_reponse_valid(response):
+                if os.path.isdir(response):
+                    kwargs['source'] = response
+                    valid = True
+                    print("Directory found")
+                else:
+                    print("Directory not found. Please try again or type [q] or [quit] and press enter.")
+                    valid = False
+    valid = False
+    while not valid:
+        if 'destination' in kwargs:
+            print("Destination: [{}]".format(kwargs['destination']), end="")
+            valid = True
+        else:
+            print("Destination ", end="")
+        response = input(" :").strip()
+        if response:
+            if is_reponse_valid(response):
+                if is_valid_folder_name(response):
+                    if not os.path.isdir(response):
+                        key = input("Folder does not exist, do with to create it? [Y]").lower()
+                        if not key == "yes" and not key == "y" and not key == "":
+                            valid = False
+                            continue
+                        else:
+                            print("Okay\n")
+                            kwargs['destination'] = response
+                            valid = True
+                else:
+                    print("Directory not found. Please try again or type [q] or [quit] and press enter.")
+                    valid = False
+
+
+
+    kwargs['object_id_prefix'] = valid_str('object_id_prefix', "Object ID MARC code", kwargs)
+    kwargs['object_id_start'] = valid_int('object_id_start', 'Object ID starting number', kwargs)
+    kwargs['project_id_prefix'] = valid_str('project_id_prefix', "Project ID MARC code", kwargs)
+    kwargs['project_id_start'] = valid_int('project_id_start', "Project ID starting number", kwargs)
+
+    return kwargs
+
+
+def display_settings(kwargs):
+    print("*****************************************************************************************")
+    print("Source Directory:                    {}".format(kwargs['source']))
+    print("Destination Directory:               {}".format(kwargs['destination']))
+    print("Project Identifier MARC Identifier:  {}".format(kwargs['project_id_prefix']))
+    print("Project Identifier Starting Number:  {}".format(kwargs['project_id_start']))
+    print("Object Identifier MARC Identifier:   {}".format(kwargs['object_id_prefix']))
+    print("Object Identifier Starting Number:   {}".format(kwargs['object_id_start']))
+    print("User Name:                           {}".format(kwargs['user']))
+    print("*****************************************************************************************")
+    pass
+
+
+def start_cli(**kwargs):
+    source = None
+    user = None
+    object_id_prefix = None
+    first_object_id = 1
+    proj_id_prefix = "CAPS"
+    first_project_id = 1
+    output_path = None
+    needs_more_info = True
+    while needs_more_info:
+        needs_more_info = False
+
+        if 'source' in kwargs:
+            source = kwargs['source']
+            if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                print("source: pass")
+        else:
+            needs_more_info = True
+
+        if 'object_id_prefix' in kwargs:
+            object_id_prefix = kwargs['object_id_prefix']
+            if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                print("object_id_prefix: pass")
+        else:
+            needs_more_info = True
+
+        if 'object_id_start' in kwargs:
+            try:
+                int(kwargs['object_id_start'])
+                first_object_id = kwargs['object_id_start']
+                if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                    print("object_id_start pass")
+            except TypeError:
+                needs_more_info = True
+        else:
+            needs_more_info = True
+
+        if 'project_id_prefix' in kwargs:
+            proj_id_prefix = kwargs['project_id_prefix']
+            if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                print("project_id_prefix: pass")
+        else:
+            needs_more_info = True
+
+        if 'project_id_start' in kwargs:
+            try:
+                int(kwargs['project_id_start'])
+                first_project_id = kwargs['project_id_start']
+                if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                    print("project_id_start: pass")
+            except TypeError:
+                needs_more_info = True
+        else:
+            needs_more_info = True
+
+        if 'user' in kwargs:
+            user = kwargs['user']
+            if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                print("user: pass")
+
+        if 'destination' in kwargs:
+            output_path = kwargs['destination']
+            if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+                print("destination: pass")
+        else:
+            needs_more_info = True
+
+        if needs_more_info:
+            kwargs = update_information(kwargs)
+            continue
+        display_settings(kwargs)
+        while True:
+            is_user_happy = input("Are these correct? [y]/n/q ")
+            if is_user_happy.lower() == 'n' or is_user_happy.lower() == 'no':
+                needs_more_info = True
+                kwargs = update_information(kwargs)
+                break
+            elif is_user_happy.lower() == 'y' or is_user_happy.lower() == 'yes' or not is_user_happy:
+                needs_more_info = False
+                break
+            elif is_user_happy.lower() == 'q' or is_user_happy.lower() == 'quit':
+                exit()
+            else:
+                sys.stderr.write("Not a valid answer. Please try again.\n")
+
+
+    if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
+        print("All data tests pass. Moving on")
     if MODE == running_mode.BUILD:
         first_object_id = 30
         first_project_id = 400
         object_id_prefix = "cusb"
         output_path = "/Volumes/CAVPPTestDrive/test/"
-
 
     builder = rename_files.renaming_model.RenameFactory()
     if MODE == running_mode.DEBUG or MODE == running_mode.BUILD:
