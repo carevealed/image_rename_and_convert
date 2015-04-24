a03c79eeaa57afe0d21cf2949d78102ee5d38b60
henryborchers <henryborchers@yahoo.com>
2015-04-23 17:39:06 -0700

    Add CLI arguments for object_id_start and project_id_start

--- rename_files/batch_renamer.py
+++ rename_files/batch_renamer.py
@@ -4,24 +4,38 @@ def main():
     parser = argparse.ArgumentParser(description="Renamer")
     parser.add_argument('-s', '--source', type=str, help="The source")
     parser.add_argument('-d', '--destination', type=str, help="Destination")
-    parser.add_argument('-o', '--object_id_prefix', type=str, help="MARC code for the institution")
+    parser.add_argument('-o', '--object_id_prefix', type=str, help="MARC code for the institution used for the object "
+                                                                   "identifier ")
+    parser.add_argument('-x', '--object_id_start', type=int, help="the starting number for the object identifier")
+
+    parser.add_argument('-p', '--project_id_prefix', type=str, default="CAPS", help="MARC code for the institution used for the project "
+                                                                    "identifer. Default: CAPS")
+    parser.add_argument('-y', '--project_id_start', type=int, help="the starting number for the object identifier")
+
     parser.add_argument('-u', '--username', type=str, help="Identify the user")
     parser.add_argument('-i', '--interactive', action='store_true', help="Interactive mode")
     parser.add_argument('-g', '--gui', action='store_true', help="Graphical User Interface mode. EXPERIMENTAL.")
     # parser.
     args = parser.parse_args()
 
-    print("SOURCE: {0}.".format(args.source))
-    print("DESTINATION: {0}.".format(args.destination))
-    print("MARC code for the institution: {0}.".format(args.object_id_prefix))
-    print("username: {0}.".format(args.username))
-    print("INTERACTIVE: {0}.".format(args.interactive))
-    print("GUI: {0}.".format(args.gui))
+    # print("SOURCE: {0}.".format(args.source))
+    # print("DESTINATION: {0}.".format(args.destination))
+    # print("MARC code for the institution: {0}.".format(args.object_id_prefix))
+    # print("username: {0}.".format(args.username))
+    # print("INTERACTIVE: {0}.".format(args.interactive))
+    # print("GUI: {0}.".format(args.gui))
 
-    if args.source and args.destination:
-        print("Starting automatic mode")
+    if args.source:
+        print("Starting CLI mode")
         import cli
-        cli.start_cli(source=args.source, obj_id_prefix=args.object_id_prefix, output_path=args.destination, user=args.username)
+        # cli.start_cli(source=args.source, obj_id_prefix=args.object_id_prefix, output_path=args.destination, user=args.username)
+        cli.start_cli(source=args.source,
+                      destination=args.destination,
+                      object_id_prefix=args.object_id_prefix,
+                      object_id_start=args.object_id_start,
+                      project_id_prefix=args.project_id_prefix,
+                      project_id_start=args.project_id_start,
+                      user=args.username)
 
     # if args.interactive:
     #     print("INTERACTIVE")
