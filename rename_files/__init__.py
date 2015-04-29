9d639898ebca7a62656130853055c814b1248d11
henryborchers <henryborchers@yahoo.com>
2015-04-29 13:26:10 -0700

    fixed rename module to run in own namespace

--- rename_files/__init__.py
+++ rename_files/__init__.py
@@ -1,1 +1,9 @@
 __author__ = 'California Audio Visual Preservation Project'
+from rename_files import renaming_model
+from rename_files import cli
+from rename_files import gui
+from rename_files import gui_data
+# __all__ = ['gui_data']
+# import rename_files.gui_data
+
+# from rename_files.gui_data.gui_view import Ui_Form
