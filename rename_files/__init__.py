705c1a2241fd3f64fe409e7044dddaeefd9ab498
henryborchers <henryborchers@yahoo.com>
2015-04-29 16:51:24 -0700

    fixed the gui import

--- rename_files/__init__.py
+++ rename_files/__init__.py
@@ -2,7 +2,7 @@ __author__ = 'California Audio Visual Preservation Project'
 from rename_files import renaming_model
 from rename_files import cli
 from rename_files import gui
-from rename_files import gui_data
+# from rename_files import gui_data
 # __all__ = ['gui_data']
 # import rename_files.gui_data
 
