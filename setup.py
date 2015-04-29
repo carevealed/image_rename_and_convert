315b07192ad891a1a94d10518aafed34b73c4acc
henryborchers <henryborchers@yahoo.com>
2015-04-29 13:28:42 -0700

    included the install_requires argument for Pyside and setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='rename_files',
-    version='0.1',
-    packages=['rename_files'],
+    version='0.1.1',
+    packages=find_packages(),
     url='https://github.com/cavpp/rename_files',
     license='GPL',
+    install_requires=['setuptools >= 14.3.1','PySide >= 1.2.2'],
     author='California Audio Visual Preservation Project',
     author_email='hborcher@berkeley.edu',
     description='Renames image files from a folder to CAVPP naming scheme',
