272e31f2e967ddcc77f0c390d7e2c86af20154a1
henryborchers <henryborchers@yahoo.com>
2015-04-30 15:57:08 -0700

    moved generate_report into renaming_model.py

--- rename_files/cli.py
+++ rename_files/cli.py
@@ -1,9 +1,8 @@
-import csv
 from enum import Enum, unique
 import os
 import sys
 import rename_files.renaming_model
-
+from rename_files.renaming_controller import generate_report
 
 
 __author__ = 'California Audio Visual Preservation Project'
@@ -217,18 +216,18 @@ def display_settings(kwargs):
     pass
 
 
-def generate_report(reporter, report_file_name):
-    records = reporter.get_last_job()
-    with open(report_file_name, 'w') as f:
-        csv_file = csv.writer(f)
-        csv_file.writerow(["#", "Project ID", "Object ID", "Original Name:", "New Name:", "MD5"])
-        # f.write(
-        #     "=============================================================================================================================================\n")
-        for index, record in enumerate(records):
-            # print(record)
-            project_id = (record['project_id_prefix'] + str(record['project_id_number']).zfill(6))
-            object_id = (record['object_id_prefix'] + "_" + str(record['object_id_number']).zfill(5))
-            csv_file.writerow([index + 1, project_id, object_id, record['source'], os.path.basename(record['destination']), record['md5']])
+# def generate_report(reporter, report_file_name):
+#     records = reporter.get_last_job()
+#     with open(report_file_name, 'w') as f:
+#         csv_file = csv.writer(f)
+#         csv_file.writerow(["#", "Project ID", "Object ID", "Original Name:", "New Name:", "MD5"])
+#         # f.write(
+#         #     "=============================================================================================================================================\n")
+#         for index, record in enumerate(records):
+#             # print(record)
+#             project_id = (record['project_id_prefix'] + str(record['project_id_number']).zfill(6))
+#             object_id = (record['object_id_prefix'] + "_" + str(record['object_id_number']).zfill(5))
+#             csv_file.writerow([index + 1, project_id, object_id, record['source'], os.path.basename(record['destination']), record['md5']])
 
 
 def start_cli(**settings):
