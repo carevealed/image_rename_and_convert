59fd31621576eff22cd3cad2ba8f841169fbe60f
henryborchers <henryborchers@yahoo.com>
2015-05-04 16:55:55 -0700

    fixed naming problem

--- rename_files/__init__.py
+++ rename_files/__init__.py
@@ -1,8 +1,9 @@
 __author__ = 'California Audio Visual Preservation Project'
-from rename_files import renaming_model
-from rename_files import cli
-from rename_files import gui
-# from rename_files import gui_data
+# from rename_files import renaming_model
+from rename_files.cli import start_cli
+from rename_files.gui import start_gui
+from rename_files import renaming_controller
+from rename_files import gui_datafiles
 # __all__ = ['gui_data']
 # import rename_files.gui_data
 
