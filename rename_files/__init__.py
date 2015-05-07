50c68ec31505e9ec32facfb4fd4f937642a810e0
henryborchers <henryborchers@yahoo.com>
2015-05-07 14:25:01 -0700

    fixed packaging problem

--- rename_files/__init__.py
+++ rename_files/__init__.py
@@ -1,9 +1,9 @@
 __author__ = 'California Audio Visual Preservation Project'
 # from rename_files import renaming_model
 from rename_files.cli import start_cli
-from rename_files.gui import start_gui
+# from rename_files.gui import start_gui
 from rename_files import renaming_controller
-from rename_files import gui_datafiles
+# from rename_files import gui_datafiles
 # __all__ = ['gui_data']
 # import rename_files.gui_data
 
