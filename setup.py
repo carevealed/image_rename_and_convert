5d01f5237caabb2437570b4d5c33a5a84eac6723
henryborchers <henryborchers@yahoo.com>
2015-05-08 16:11:03 -0700

    upped a point

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='rename_files',
-    version='0.1.11',
+    version='0.2',
     packages=find_packages(),
     url='https://github.com/cavpp/rename_files',
     license='GPL',
-    install_requires=['setuptools >= 14.3.1','PySide >= 1.2.2'],
+    install_requires=['setuptools >= 14.3.1','PySide >= 1.2.2', 'Pillow >=2.8.1'],
     author='California Audio Visual Preservation Project',
     author_email='hborcher@berkeley.edu',
     description='Renames image files from a folder to CAVPP naming scheme',
