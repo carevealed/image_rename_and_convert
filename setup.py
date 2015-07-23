a0ca08246a687a4e115193057ab0b114f86bf937
henryborchers <henryborchers@yahoo.com>
2015-07-23 16:30:24 -0700

    added included package data

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='rename_and_convert_images',
-    version='0.5b3',
+    version='0.5b4',
     packages=['rename_files', 'rename_files.gui_datafiles', 'rename_files.data'],
     package_data={'data': ['data.db']},
+    include_package_data=True,
     url='https://github.com/cavpp/rename_files',
     license='GPL',
     install_requires=['setuptools >= 14.3.1', 'Pillow >=2.8.1'],
