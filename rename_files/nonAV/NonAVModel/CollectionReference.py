90b2506db5ca843cf2dae62282a47282ee88ca28
henryborchers <henryborchers@yahoo.com>
2015-07-30 18:04:47 -0700

    Removed the DescriptionDocument() in the constructor so that it can tell if it doesn't have a DescriptionDocument attached.

--- rename_files/nonAV/NonAVModel/CollectionReference.py
+++ rename_files/nonAV/NonAVModel/CollectionReference.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-from .DescriptionDocument import DescriptionDocument
+# from .DescriptionDocument import DescriptionDocument
 from .CAPS_node import CAPS_node
 from .Element import Element
 
 class CollectionReference(CAPS_node):
     def __init__(self, collectionSource=None, projectSource=None):
         super(CollectionReference, self).__init__()
-        self._descriptionDocument = DescriptionDocument()
+        self._descriptionDocument = None
         self._collectionSource = None
         self._projectSource = None
 
         if collectionSource:
-            self.collectionSource = collectionSource
+            self._collectionSource = collectionSource
         if projectSource:
-            self.projectSource = projectSource
+            self._projectSource = projectSource
 
     def _make_xml(self):
         root = Element(tag="CollectionReference")
-        root.add_attribute("collectionSource", self.collectionSource)
-        root.add_attribute("projectSource", self.projectSource)
+        root.add_attribute("collectionSource", self._collectionSource)
+        root.add_attribute("projectSource", self._projectSource)
         root.add_attribute("xmlns", "http://calpreservation.org/projects/audiovisual-preservation/dcscheme.html")
         root.add_attribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
         root.add_attribute("xsi:schemaLocation", "http://calpreservation.org/projects/audiovisual-preservation/dcscheme.html http://calpreservation.org/projects/audiovisual-preservation/CAVPPDC.xsd")
 
-        root.add_child(self.descriptionDocument)
+        root.add_child(self._descriptionDocument)
         return root
 
     def check_required_data(self):
@@ -32,12 +32,12 @@ class CollectionReference(CAPS_node):
         missing_fields = []
         valid = False
 
-        if not self.collectionSource:
+        if not self._collectionSource:
             missing_attributes.append("collectionSource")
-        if not self.projectSource:
+        if not self._projectSource:
             missing_attributes.append("projectSource")
 
-        if not self.descriptionDocument:
+        if not self._descriptionDocument:
             missing_fields.append("DescriptionDocument")
 
         if len(missing_fields) == 0 and len(missing_attributes) == 0:
